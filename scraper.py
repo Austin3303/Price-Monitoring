0380d5f47f4fbe66a4427be870c8f8e5b43150f7
Austin3303 <119144981+Austin3303@users.noreply.github.com>
2026-04-03 16:20:08 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ ITEMS = [
     {
         "name": "GW Gentle Pace Cap",
         "url": "https://www.gentlewomanonline.com/product/1103913",
-        "selector": ".product-price",
+        "selector": ".w-fit.pr-3.text-center.font-medium span"
         "js": True
     },
 ]
